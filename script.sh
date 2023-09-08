9e3e0231e39444ff5956dd23a90af00cf94a5f2f
Evgeny Azeniz <Azenizzka@gmail.com>
2023-09-08 20:20:32 +0700

    fix problems

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Install
-sudo pacman -S git curl yay base-devel gdb ninja gcc cmake libxcb xcb-proto xcb-util  
-xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender  
-pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm  
-xorg-xwayland cmake wlroots mesa meson polkit hyprland hyprpaper waybar
-capitaine-cursorsfish alacritty wofi firefox discord noto-fonts-emoji noto-fonts-extra
-pulseaudio telegram-desktop
+sudo pacman -S mesa meson polkit hyprland hyprpaper waybar capitaine-cursors fish
+
+sudo pacman -S git curl yay base-devel gdb ninja gcc cmake libxcb xcb-proto xcb-util
+
+sudo pacman -S xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender
+
+sudo pacman -S pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland cmake wlroots
+
+sudo pacman -S alacritty wofi firefox discord noto-fonts-emoji noto-fonts-extra pulseaudio telegram-desktop
 
 gsettings set org.gnome.desktop.interface cursor-theme capitaine-cursors 
 
